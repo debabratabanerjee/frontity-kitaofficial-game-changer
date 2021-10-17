f9df7f858ba8351d2d7b59faf3045c52c19019c3
debabratabanerjee <debabratabanerjee021@gmail.com>
2021-10-17 11:12:06 +0530

    Added kita site url

--- frontity.settings.js
+++ frontity.settings.js
@@ -2,7 +2,7 @@ const settings = {
   "name": "kita-official",
   "state": {
     "frontity": {
-      "url": "https://test.frontity.org",
+      "url": "https://kitaofficial.com",
       "title": "Test Frontity Blog",
       "description": "WordPress installation for Frontity development"
     }
@@ -45,7 +45,7 @@ const settings = {
       "name": "@frontity/wp-source",
       "state": {
         "source": {
-          "url": "https://test.frontity.org"
+          "url": "https://kitaofficial.com"
         }
       }
     },
